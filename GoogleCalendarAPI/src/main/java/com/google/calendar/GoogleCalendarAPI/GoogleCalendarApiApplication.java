04bf4d5d1bb3d25936a5204734c2fdf9f18884d5
vishalcode <mewativishal@gmail.com>
2022-08-17 16:04:14 +0530

    first commit

---
+++ GoogleCalendarAPI/src/main/java/com/google/calendar/GoogleCalendarAPI/GoogleCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package com.google.calendar.GoogleCalendarAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCalendarApiApplication.class, args);
+	}
+
+}
